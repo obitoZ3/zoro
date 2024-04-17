fe68951002217c5f9a0338d926a1b9406bcb94df
obitoZ3 <167070237+obitoZ3@users.noreply.github.com>
2024-04-17 10:15:47 +0530

    Create word.py

---
+++ word.py
@@ -0,0 +1,17 @@
+word=input()
+print(word)
+
+store=dict()
+print(store)
+
+store['p']=23
+store['q']=12
+store[12]=11
+store["hello"]=222
+print(store)
+
+allkeys=store.keys()
+print(allkeys)
+
+for ele in allkeys:
+    print(ele)
